945f3a64de82cb62cd60aa46ab61af080a99177f
Todd <toddtokyo3@gmail.com>
2026-07-04 17:46:10 +0900

    Fix: latest 100 MSGs kept

--- ChatApp.py
+++ ChatApp.py
@@ -454,4 +454,4 @@ def handle_send_message(data):
 # アプリケーションの起動（Renderなど外部公開用に0.0.0.0ポートを指定）
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
